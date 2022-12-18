b5691085cab27f47ab6c7c8a900c172bffaaa99e
Austin Rainwater <hey@paco.wtf>
2022-12-18 12:27:55 -0500

    Move other effects to C++ file

--- esphome/christmas_tree.h
+++ esphome/christmas_tree.h
@@ -19,6 +19,12 @@ Color effect_color(int color_id) {
     return Color(r*255, g*255, b*255);
 }
 
+void do_solid_color_effect(AddressableLight &it, int num_colors = 4) {
+    for (int i = it.size() - 1; i >= 0; i--) {
+        it[i] = effect_color(i % num_colors);
+    }
+}
+
 void do_marquee_effect(AddressableLight &it, int num_colors = 4) {
     const uint16_t transition_time = 500;
 
@@ -32,5 +38,33 @@ void do_marquee_effect(AddressableLight &it, int num_colors = 4) {
 
     for (int i = it.size() - 1; i >= 0; i--) {
         it[i] = effect_color((i + offset) % num_colors);
+    }
+}
+
+void do_solid_fade_effect(AddressableLight &it, int num_colors = 4) {
+    const uint16_t static_time = 5000;
+    const uint16_t transition_time = 3000;
+
+    static uint32_t next_state = millis() + static_time;
+    static uint8_t transition_state = false;
+    static uint8_t color_state = 0;
+
+    uint32_t now = millis();
+
+    if (!transition_state && now < next_state) {
+        it.all() = effect_color(color_state);
+    } else if (!transition_state) {
+        transition_state = true;
+        next_state = now + transition_time;
+    }
+
+    if (transition_state && now < next_state) {
+        float gradient_amount = (float) (transition_time - (next_state - now)) / transition_time;
+        Color new_color = effect_color(color_state).gradient(effect_color((color_state + 1) % num_colors), gradient_amount * 256);
+        it.all() = new_color;
+    } else if (transition_state) {
+        color_state = (color_state + 1) % num_colors;
+        transition_state = false;
+        next_state = now + static_time;
     }
 }
