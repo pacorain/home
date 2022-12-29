6bbe7f09f5b5af6ec7a268ee1024c02e71ebb3ae
Austin Rainwater <hey@paco.wtf>
2022-12-28 21:11:50 -0500

    More Chrostimas Tree stuff

--- esphome/christmas_tree.h
+++ esphome/christmas_tree.h
@@ -68,3 +68,35 @@ void do_solid_fade_effect(AddressableLight &it, int num_colors = 4) {
         next_state = now + static_time;
     }
 }
+
+void do_cycle_effect(AddressableLight &it, int num_colors = 4) {
+    const uint16_t cycle_time = 8000;
+
+    static uint32_t cycle_start = millis();
+
+    uint16_t num_lights = it.size();
+    uint32_t now = millis();
+    Color color1, color2;
+    uint16_t light_state;
+    uint8_t color_step;
+    uint16_t step_state;
+    float gradient_amount;
+
+    if (now > cycle_start + cycle_time) {
+        cycle_start = now;
+    }
+
+    uint16_t cycle_offset = now - cycle_start;
+    uint16_t step_time = ceil(cycle_time / num_colors);
+
+    for (int i = it.size() - 1; i >= 0; i--) {
+        light_state = (cycle_offset + (i * floor(cycle_time / num_lights))) % cycle_time;
+        color_step = (uint8_t) (num_colors * light_state / cycle_time);
+        color1 = effect_color(color_step);
+        color2 = effect_color((color_step + 1) % num_colors);
+        
+        step_state = light_state % step_time;
+        gradient_amount = max(min((float) ((step_state * 2) - (step_time)) / step_time, 1.0f), 0.0f);
+        it[i] = color1.gradient(color2, (float) 256 * gradient_amount);
+    }
+}
